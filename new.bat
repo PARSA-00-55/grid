e88b4af1fc77ad818cdbbbe9610ab68809558845
PARSA-00-55 <parsa02345@gmail.com>
2022-07-17 21:49:57 +0600

    first commit

---
+++ new.bat
@@ -0,0 +1,7 @@
+
+@echo off
+color 0a
+title ---------------Matrix Virus---------------
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
